ed67a3de300d5793a4696fb3167ad721646d6396
Fengqiwan <zsfengqiwan@163.com>
2026-04-24 15:29:04 +0800

    Update load.js

--- js/load.js
+++ js/load.js
@@ -4,7 +4,6 @@ function loadHTML(elementId, filePath) {
     .then(data => document.getElementById(elementId).innerHTML = data);
 }
 
-loadHTML('head', '../head.html');
 loadHTML('header', '../header.html');
 loadHTML('footer', '../footer.html');
 
