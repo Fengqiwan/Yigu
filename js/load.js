39dfc32bb2cfd72d7cb4d5d61d21617d7fc9176a
Fengqiwan <zsfengqiwan@163.com>
2026-04-24 14:25:50 +0800

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,10 @@
+function loadHTML(elementId, filePath) {
+  fetch(filePath)
+    .then(res => res.text())
+    .then(data => document.getElementById(elementId).innerHTML = data);
+}
+
+loadHTML('header', '../header.html');
+loadHTML('footer', '../footer.html');
+
+// 此相对路径仅适用于pages,index不适用
